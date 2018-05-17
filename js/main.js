e7ab37ae01ebe5b0d02fb2ab85107af08035b1be
Tina <tparkmedia@gmail.com>
2018-05-17 12:30:44 -0700

    animated list styles

--- js/main.js
+++ js/main.js
@@ -11,5 +11,17 @@ $form.on('submit', function(evt){
     var bathroomValue = $bathrooms.val()
     var bedroomsValue = $bedrooms.val()
     console.log(addressValue)
+
+    $list.append(`<li style="display:none">Address: ${addressValue} Bedrooms: ${bedroomsValue} Bathrooms: ${bathroomValue}</li>`)
+    $('li').last().fadeIn()
+})
+
+    // if user clicks anywhere in this list
+    // 'this' refers to the specific thing within
+    // that was clicked
+    $list.on('click', 'li', function() {
+        $(this).fadeOut(function () {
+        $(this).remove()
+    })
 })
 
